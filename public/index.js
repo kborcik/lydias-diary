62a0d5d6791600f1065553bad046000f05481dda
Kyle Borcik <kyleborcik@gmail.com>
2023-03-02 16:13:42 -0500

    first iteratin of lydias diary

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+
+form.addEventListener('click', submitDiary)
+
+
+const submit = (evt) =>{
+    evt.preventDefault()
+axios.post()
+    document.querySelector('textarea')
+}
