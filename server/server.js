d3850b42ca3efb2ebab7bde4dc8ec8dc36ebff2e
Kyle Borcik <kyleborcik@gmail.com>
2023-03-02 16:42:52 -0500

    second iteration

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(express(json))
+app.use(express.json())
 app.use(cors)
 
 const path= require('path')
 
 
+app.listen(4000, ()=>{console.log('app running on 4000')})
